76ccb7798feeba12814a83c00698989852772bd4
Fernando Palomo Garcia <fernando@bluejay.es>
2013-03-03 13:32:27 +0100

    not necessary LoC

--- mergePullRequest.php
+++ mergePullRequest.php
@@ -10,8 +10,6 @@ $params["password"] = isset($argv[2]) ? $argv[2] : null;
 $params["owner"] = isset($argv[3]) ? $argv[3] : null;
 $params["repo"] = isset($argv[4]) ? $argv[4] : null;
 
-$configFile = "./Config/Config.yaml";
-
 $app = new App();
 
 $merge =  new Command\Merge(App::Config());
